528076bb6c14a8a33b271306010c27a55d919db4
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-06-12 17:29:44 +0300

    refactore: Move the date managing logic from the App component to the booking page

--- src/App.js
+++ src/App.js
@@ -1,58 +1,13 @@
-import { useCallback, useEffect, useReducer } from "react";
 import { RouterProvider, createHashRouter } from "react-router-dom";
 import Booking from "./pages/Booking";
 import Home from "./pages/Home";
 import Root from "./pages/Root";
 import ReservationsPage from "./pages/Reservations";
-
-import {
-  fetchAPI,
-  submitAPI,
-} from "./JavaScript API file/raw.githubusercontent.com_Meta-Front-End-Developer-PC_capstone_master_api";
 import Confirmation from "./pages/Confirmation";
 import AboutPage from "./pages/About";
 import ErrorPage from "./pages/ErrorPage";
 
-const reducer = (state, action) => {
-  if (action.type === "initialTimes") {
-    const initTimes = action.initialTimes;
-    return [...initTimes];
-  }
-  if (action.type === "dateUpdate") {
-    const newTimes = action.times;
-    return [...newTimes];
-  }
-
-  return state;
-};
-
 function App() {
-  const [availableTimes, dispatch] = useReducer(reducer, []);
-
-  const initializeTimes = (initialTimes) => {
-    dispatch({ type: "initialTimes", initialTimes: initialTimes });
-  };
-
-  const updateTimes = useCallback((times) => {
-    dispatch({ type: "dateUpdate", times: times });
-  }, []);
-
-  const submitForm = async (formData) => {
-    const response = await submitAPI(formData);
-
-    return response;
-  };
-
-  const fetchData = useCallback(async () => {
-    const date = new Date();
-    const times = await fetchAPI(date);
-    initializeTimes(times);
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
   const router = createHashRouter([
     {
       path: "/",
@@ -66,13 +21,7 @@ function App() {
         },
         {
           path: "booking",
-          element: (
-            <Booking
-              availableTimes={availableTimes}
-              setAvailableTimes={updateTimes}
-              submitForm={submitForm}
-            />
-          ),
+          element: <Booking />,
           errorElement: <ErrorPage />,
         },
         {
--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,8 +1,51 @@
+import { useReducer, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import BookingForm from "../components/Booking/BookingForm";
+import {
+  fetchAPI,
+  submitAPI,
+} from "../JavaScript API file/raw.githubusercontent.com_Meta-Front-End-Developer-PC_capstone_master_api";
+
+const reducer = (state, action) => {
+  if (action.type === "initialTimes") {
+    const initTimes = action.initialTimes;
+    return [...initTimes];
+  }
+  if (action.type === "dateUpdate") {
+    const newTimes = action.times;
+    return [...newTimes];
+  }
+
+  return state;
+};
 
 const Booking = (props) => {
+  const [availableTimes, dispatch] = useReducer(reducer, []);
   const navigate = useNavigate();
+
+  const fetchData = useCallback(async () => {
+    const date = new Date();
+    const times = await fetchAPI(date);
+    initializeTimes(times);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const initializeTimes = (initialTimes) => {
+    dispatch({ type: "initialTimes", initialTimes: initialTimes });
+  };
+
+  const updateTimes = useCallback((times) => {
+    dispatch({ type: "dateUpdate", times: times });
+  }, []);
+
+  const submitForm = async (formData) => {
+    const response = await submitAPI(formData);
+
+    return response;
+  };
 
   const navigateHandler = (path) => {
     navigate(path);
@@ -11,9 +54,9 @@ const Booking = (props) => {
   return (
     <BookingForm
       navigate={navigateHandler}
-      availableTimes={props.availableTimes}
-      setAvailableTimes={props.setAvailableTimes}
-      submitForm={props.submitForm}
+      availableTimes={availableTimes}
+      setAvailableTimes={updateTimes}
+      submitForm={submitForm}
     />
   );
 };
